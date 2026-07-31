0e1a45775dd70c19cc64f0bde459e0947072baa1
Fenhl <fenhl@fenhl.net>
2026-07-31 15:09:58 +0000

    Exclude assets from mod package

--- assets/package.sh
+++ assets/package.sh
@@ -4,4 +4,4 @@ set -e
 
 version=$(jq -r '.version' info.json)
 rm -f rubber-ducky-fenhl_*.zip
-git archive --prefix "rubber-ducky-fenhl_${version}/" -o "rubber-ducky-fenhl_${version}.zip" HEAD
+git archive --prefix "rubber-ducky-fenhl_${version}/" -o "rubber-ducky-fenhl_${version}.zip" HEAD . ':!/assets'
